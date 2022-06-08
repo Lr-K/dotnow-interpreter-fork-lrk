faf7a69d3dcf3dfc6d2f2acc2647ed1f378f0d2f
Le-Roy Karunaratne <lrkaru@gmail.com>
2022-06-08 12:10:13 -0700

    Added direct call bindings for particle system

---
+++ Assets/dotnow/Scripts/Integration/Unity/DirectCallBindings.ParticleSystem.cs
@@ -0,0 +1,29 @@
+﻿using dotnow;
+using dotnow.Runtime;
+using UnityEngine;
+
+internal static partial class DirectCallBindings
+{
+    [Preserve]
+    [CLRMethodDirectCallBinding(typeof(ParticleSystem), "get_isPlaying")]
+    public static void unityEngine_ParticleSystem_IsPlaying(StackData[] stack, int offset)
+    {
+        stack[offset].refValue = ((ParticleSystem)stack[offset].refValue).isPlaying;
+        stack[offset].type = StackData.ObjectType.Ref;
+    }
+
+    [Preserve]
+    [CLRMethodDirectCallBinding(typeof(ParticleSystem), "set_emissionRate", typeof(float))]
+    public static void unityEngine_ParticleSystem_SetEmissionRate(StackData[] stack, int offset)
+    {
+        ((ParticleSystem)stack[offset].refValue).emissionRate = stack[offset + 1].value.Single;
+    }
+
+    [Preserve]
+    [CLRMethodDirectCallBinding(typeof(ParticleSystem), "get_emissionRate")]
+    public static void unityEngine_ParticleSystem_GetEmissionRate(StackData[] stack, int offset)
+    {
+        stack[offset].refValue = ((ParticleSystem)stack[offset].refValue).emissionRate;
+        stack[offset].type = StackData.ObjectType.Ref;
+    }
+}
