0f60a0a6c533228eede4d78f62c4a93cb6591cb4
Le-Roy Karunaratne <lrkaru@gmail.com>
2022-06-08 12:10:20 -0700

    Added direct call bindings for mesh filter

---
+++ Assets/dotnow/Scripts/Integration/Unity/DirectCallBindings.MeshFilter.cs
@@ -0,0 +1,14 @@
+﻿using dotnow;
+using dotnow.Runtime;
+using UnityEngine;
+
+internal static partial class DirectCallBindings
+{
+    [Preserve]
+    [CLRMethodDirectCallBinding(typeof(MeshFilter), "get_mesh")]
+    public static void unityEngine_MeshFilter_GetMesh(StackData[] stack, int offset)
+    {
+        stack[offset].refValue = ((MeshFilter)stack[offset].refValue).mesh;
+        stack[offset].type = StackData.ObjectType.Ref;
+    }
+}
